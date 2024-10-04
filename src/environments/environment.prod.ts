8f201f632d9830c5075c787058a02eb19c83850e
Edgar Vargas <vargasbiniegra@gmail.com>
2024-10-04 11:14:47 -0600

    treceavo commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://api-rest-latest.onrender.com'
-}
+//export const environment = {
+  //  production: true,
+  //  apiUrl: 'https://api-rest-latest.onrender.com/alumnos'
+//}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     production: false
     , apiUrl: 
     //'http://localhost:9000/alumnos'
-    'http://localhost:8080/alumnos'
+    //'http://localhost:8080/alumnos'
+    'https://api-rest-latest.onrender.com/alumnos'
 }; 
